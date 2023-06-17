211011cee5c081d0de55ff8c765ae8c73a704d3a
MorphyKutay <kutaynevzat2006@gmail.com>
2023-06-17 15:58:29 +0300

    commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import time
+
+# degiskenler
+apikey = "grep -i 'google_api_key' "
+
+#-------------
+apk = input("apk ismini giriniz : ")
+
+print("apk decompile ediliyor...")
+time.sleep(5)
+
+os.system("apktool d "+apk)
+
+path = apk.strip('.apk')
+
+value = path+"/res/values/strings.xml"
+
+with open(value, "r") as file:
+    content = file.read()
+    if "google_api_key" in content:
+        start_index = content.index("google_api_key") + len("google_api_key") + 1
+        end_index = content.find("\n", start_index)
+        api_key = content[start_index:end_index].strip()
+        print("API anahtarı:", api_key)
+    else:
+         raise ValueError("Hata: API anahtarı bulunamadı!")
